2c3406078de90dead1ae48559b4da79fda3042e0
Bart <frontendbart@gmail.com>
2024-06-25 20:56:17 +0100

    queries

--- src/components/PickCategory.jsx
+++ src/components/PickCategory.jsx
@@ -13,14 +13,18 @@ const PickCategory = ({ onClick, onCategoryPick }) => {
     "Sports",
   ];
   return (
-    <main className={styles.outerWrapper}>
+    <div className={styles.outerWrapper}>
       <div className={styles.innerWrapper}>
         <div className={styles.topFlex}>
           <button onClick={onClick} className={styles.backButton}>
-            <img src={backIcon} alt="Back Icon" />
+            <img
+              className={styles.backButtonIcon}
+              src={backIcon}
+              alt="Back Icon"
+            />
           </button>
           <h1 className={styles.topFlexTitle}>Pick a Category</h1>
-          <div style={{ height: "9.4rem", width: "9.4rem" }}></div>
+          <div className={styles.divInv}></div>
         </div>
         <div className={styles.bottomGrid}>
           {categories.map((category, i) => (
@@ -32,7 +36,7 @@ const PickCategory = ({ onClick, onCategoryPick }) => {
           ))}
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
